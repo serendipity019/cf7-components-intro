14f1db36a62dfdf7c7c7ceae263ad445b188a7af
serendipity019 <navagio09@gmail.com>
2025-06-05 14:57:08 +0300

    feat: we make React BrowserRouter

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout"
+
+const HomePage = () => { 
+    return (
+        <> 
+          <h1 className="text-center text-bold mt-8 text-xl ">Welcome to the Home Page</h1>
+        </>
+    );
+}
+
+export default HomePage;
---
+++ src/pages/NameChangerPage.tsx
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout'; 
+import NameChangerComponent from "../components/NameChangerComp";
+
+const NameChangerPage = () => { 
+    return ( 
+        <>
+           <NameChangerComponent/>   
+        </>
+    );
+}
+
+export default NameChangerPage;
